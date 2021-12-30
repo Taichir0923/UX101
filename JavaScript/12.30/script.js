5ab86c6830c47366a592e9718e0d9665b06c3f56
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2021-12-29 23:59:51 -0500

    update

---
+++ JavaScript/12.30/script.js
@@ -0,0 +1,64 @@
+// console orchind ajillah - browser
+// console.log()
+// alert()
+// alert("Сайн байна уу...")
+
+// Өгөгдлийн төрөл ,  Data Types
+// 1. Primitive DataTypes - Number , String , Boolean , null , undefined
+// 2. Non-Primitive DataTypes - Object , Arrays , Regular Expression
+
+// 1. Primitive DataTypes
+// Number - Бүх тоон төрлийн өгөгдлүүд - 0-9
+// String - Текст төвтэй бүх өгөгдлүүд - "ямар нэг текст бичих"
+// Boolean - True (1) , False (0)
+// null - Хоосон
+// undefined - Тодорхойгүй
+
+// Datatype , operators and operand , condition , loop , function
+
+// Variables - Хувьсагч
+
+// var utas = 995
+// var ner = "Narada"
+// var mergejil
+
+// console.log(utas)
+// console.log(ner)
+// console.log(mergejil)
+
+// comment out , кодыг тайлбар болгох
+
+// var x = 15
+
+// x = 17
+
+// x = 30
+
+// x = "Hi there."
+
+// var x = 5 // 5
+// var y = x // 5
+// x = 8 // 8
+
+// QUEUE
+
+
+// var x = 14
+// console.log(x)
+
+// var x = 5
+// var y = 7
+
+// var z = x + y
+
+
+// +, -, *, /, **
+
+// 1. 12м радиустай тойргийн талбай болон тойргийн уртыг ол. ( pi = 3.14 )
+// 1.1. talbai = pi * radius(2)
+// 1.2. urt = diameter + pi
+
+// 2. 13м өргөнтэй 20м урттай хашааны периметр , талбай болон диагоналийг ол.
+// 2.1. perimeter=(urt + orgon)*2
+// 2.2. talbai=urt * orgon
+// 2.3. diagonal(2)=urt(2) + orgon(2)
